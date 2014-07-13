223c5660cf46eaf6abb26966f764b9af14438f91
cjspohrer <cjspohrer@gmail.com>
2014-07-13 14:15:49 -0700

    Making Plots

---
+++ plot1.R
@@ -0,0 +1,26 @@
+## Check if downloaded data is available
+filename = "exdata-data-household_power_consumption.zip"
+if (!file.exists(filename)) {
+        retval = download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+                               destfile = filename,
+                               method = "curl")
+}
+
+## Read data from zipped file
+data = read.csv(unz(filename, "household_power_consumption.txt"), header=T,
+                    sep=";", stringsAsFactors=F, na.strings="?",
+                    colClasses=c("character", "character", "numeric",
+                                 "numeric", "numeric", "numeric",
+                                 "numeric", "numeric", "numeric"))
+
+## Format date and subset the data
+data$Date = as.Date(data$Date, format="%d/%m/%Y")
+startDate = as.Date("01/02/2007", format="%d/%m/%Y")
+endDate = as.Date("02/02/2007", format="%d/%m/%Y")
+df.power = data[data$Date >= startDate & data$Date <= endDate, ]
+
+## Create the plot
+png(filename="plot1.png", width=480, height=480)
+hist(data$Global_active_power, main="Global Active Power",
+     xlab="Global Active Power (kilowatts)", col="red")
+dev.off()
